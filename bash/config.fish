ad663d13149578f58cce85f5c54e871834cec688
liangshan <liang.shan@qianka.com>
2020-01-08 14:07:53 +0800

    fish config  配置文件

---
+++ bash/config.fish
@@ -0,0 +1,5 @@
+set -x JAVA_HOME (/usr/libexec/java_home)
+set -x GOPATH /Users/liangshan/apps/go
+set -x HOMEBREW_BOTTLE_DOMAIN https://mirrors.aliyun.com/homebrew/homebrew-bottles
+alias cat='bat'
+alias ping='prettyping --nolegend'
